0c8b8fa1d0354a657965898deaf47ccac4488d92
AlexFreshIce <alexfreshice@yandex.ru>
2023-03-02 03:36:51 +0300

    add tile event click, add dell and refill fu

--- assets/Scripts/GameManager.ts
+++ assets/Scripts/GameManager.ts
@@ -1,13 +1,25 @@
-import { _decorator, Component, Node } from "cc";
+import {
+  _decorator,
+  Component,
+  Node,
+  input,
+  Input,
+  EventMouse,
+  EventTarget,
+} from "cc";
 import { TilesManager } from "./TilesManager";
 const { ccclass, property } = _decorator;
 
 @ccclass("GameManager")
 export class GameManager extends Component {
   @property({ type: TilesManager })
-  private titles: TilesManager | null = null;
+  private GameField: TilesManager | null = null;
 
-  start() {}
+  start() {
+    
+  }
+
+ 
 
   update(deltaTime: number) {}
 }
--- assets/Scripts/TilesManager.ts
+++ assets/Scripts/TilesManager.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Prefab, instantiate } from "cc";
+import { _decorator, Component, Prefab, instantiate, Node } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("TilesManager")
@@ -14,11 +14,33 @@ export class TilesManager extends Component {
   @property({ type: Prefab })
   public tileYellow: Prefab | null = null;
 
-  public arrTitles: any = [];
+  public arrTiles: any[] = [];
+  public arrWillDestroyTiles: any[] = [];
+  public needRefillArr: boolean = false;
+  public needRegenerateTiles: boolean = false;
 
-  createRandomTile = () => {
+  fillArrTiles = (firstTime: boolean = false): void => {
+    for (let i = 0; i < 9; i++) {
+      firstTime ? (this.arrTiles[i] = []) : null;
+      for (let j = 0; j < 9; j++) {
+        // console.log(`i = ${i}, j = ${j}`);
+        if (!this.arrTiles[i][j]) {
+          const tileType = this.createRandomTileType();
+          this.arrTiles[i][j] = {
+            type: tileType,
+            col: i,
+            row: j,
+            checked: false,
+            drawStatus: false,
+          };
+        } else continue;
+      }
+    }
+    this.needRegenerateTiles = true;
+  };
+
+  createRandomTileType = (): string => {
     const color = Math.floor(Math.random() * 5);
-    console.log(color);
     switch (color) {
       case 0:
         return "tileBlue";
@@ -33,42 +55,25 @@ export class TilesManager extends Component {
     }
   };
 
-  fillArrTitles = () => {
+  generateTiles = (): void => {
     for (let i = 0; i < 9; i++) {
-      this.arrTitles[i] = [];
       for (let j = 0; j < 9; j++) {
         // console.log(`i = ${i}, j = ${j}`);
-        // if (!(arr[i][j])) {
-        const tileType = this.createRandomTile()
-        this.arrTitles[i][j] = {
-          type: tileType,
-          col: i,
-          row: j,
-          checked: false,
-        };
-        // } else continue;
+        if (!this.arrTiles[i][j].drawStatus) {
+          const tile = this.spawnTileByType(this.arrTiles[i][j].type);
+          this.node.addChild(tile);
+          this.setTilePos(tile, i, j);
+          tile.curNumber = `${i}${j}`;
+          tile.on(Node.EventType.TOUCH_START, this.onTilePress, this);
+          this.arrTiles[i][j].drawStatus = true;
+        } else continue;
       }
     }
   };
 
-
-  generateTiles = () => {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        // console.log(`i = ${i}, j = ${j}`);
-        // if (arr[i][j]) {
-        const tile = this.spawnTileByType(this.arrTitles[i][j].type);
-        this.setTilePos(tile, i, j);
-        tile.setScale(0.25, 0.25, 0);
-        this.node.addChild(tile);
-        // } else continue;
-      }
-    }
-  };
-
-  setTilePos = (tile, i, j) => {
-    const x = j*46 + 21 ;
-    const y = -(i * 50)+11;
+  setTilePos = (tile: Node, i: number, j: number) => {
+    const x = j * 46 + 32;
+    const y = -(i * 50) - 39;
     tile.setPosition(x, y, 0);
   };
 
@@ -87,15 +92,88 @@ export class TilesManager extends Component {
     }
   };
 
-  onLoad() {
-    this.fillArrTitles();
-  }
-  
-  start() {
-    this.generateTiles();
-    console.log(this.arrTitles);
-    console.log(this.node);
+  findSameColorBorderTile = (
+    col: number,
+    row: number,
+    type: string,
+    cb: () => void | null = null
+  ) => {
+    if (col < 0 || col > 8 || row < 0 || row > 8) {
+      return;
+    }
+    const tile = this.arrTiles[col][row];
+    if (!tile.checked && tile.type === type) {
+      this.arrWillDestroyTiles.push(tile);
+      tile.checked = true;
+      this.findSameColorBorderTile(col + 1, row, type);
+      this.findSameColorBorderTile(col - 1, row, type);
+      this.findSameColorBorderTile(col, row + 1, type);
+      this.findSameColorBorderTile(col, row - 1, type);
+    }
+    cb ? cb() : null;
+    // console.log(col, row, type);
+  };
+
+  onTilePress(event: any): void {
+    const col = +event.target.curNumber[0];
+    const row = +event.target.curNumber[1];
+    const type = event.target.name;
+    this.arrWillDestroyTiles = [];
+    // event.target.destroy()
+    // console.log(col, row, type);
+    console.log(event.target);
+    const cb = (): void => {
+      if (this.arrWillDestroyTiles.length >= 2) {
+        this.destroyTiles();
+      } else {
+        this.arrWillDestroyTiles.forEach((tile) => {
+          tile.checked = false;
+        });
+      }
+    };
+    this.findSameColorBorderTile(col, row, type, cb);
+
+    console.log(this.arrWillDestroyTiles);
+    // console.log(this.arrTiles);
   }
 
-  update(deltaTime: number) {}
+  destroyTiles = (): void => {
+    this.arrWillDestroyTiles.forEach((item) => {
+      const { col, row } = item;
+      this.arrTiles[col][row] = null;
+      const curNumber = `${col}${row}`;
+      const index = this.node.children.findIndex(
+        (i) => i.curNumber === curNumber
+      );
+      this.node.children[index].destroy();
+    });
+    // console.log(this.arrTiles);
+    this.needRefillArr = true;
+  };
+
+  onLoad() {
+    this.fillArrTiles(true);
+    if (this.needRegenerateTiles) {
+      this.needRegenerateTiles = false;
+      this.generateTiles();
+    }
+  }
+
+  start() {}
+
+  update(deltaTime: number) {
+    if (this.needRefillArr) {
+      this.needRefillArr = false;
+      this.fillArrTiles();
+      // setTimeout(this.fillArrTiles, 1);
+      // console.log("fill");
+    }
+    if (this.needRegenerateTiles) {
+      this.needRegenerateTiles = false;
+      this.generateTiles();
+      // console.log("gener");
+      // console.log(this.arrTiles);
+      // setTimeout(this.generateTiles, 1);
+    }
+  }
 }
